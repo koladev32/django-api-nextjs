a4f51668ba2664a64bb2853a1225a92e6bd5fa29
koladev <onaelmangao@gmail.com>
2023-10-25 22:06:24 +0100

    🐛 fix bug by adding cors headers package

--- RestaurantCore/settings.py
+++ RestaurantCore/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     #third apps
     "rest_framework",
+    "corsheaders",
+
 
     # installed apps
 
@@ -49,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -128,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
